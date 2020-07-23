d90ef9ae04b1ac810e69886572c9af8445a3a4f7
Teak Slack <teakislack@gmail.com>
2020-07-23 01:29:39 -0400

    emboss cmd

---
+++ commands/image/emboss.js
@@ -0,0 +1,19 @@
+const util = require('../../util/imageutil');
+
+module.exports.name = 'emboss';
+module.exports.description =
+  'Emboss an inputted image through a convolution matrix.';
+module.exports.usage = 'emboss <image url or attached image>';
+module.exports.category = 'image';
+
+module.exports.run = async (client, message, args, user) => {
+  let image = await util.getImage(message, args, user);
+  image.convolute([
+    [-2, -1, 0],
+    [-1, 1, 1],
+    [0, 1, 2]
+  ]);
+
+  let buffer = await util.sendImage(image, message);
+  util.cacheLastImage(buffer, user);
+};
